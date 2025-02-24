42aa0a8c2e622fb035b8de7eaa03131ede2cba6e
Gailyy <120695735+kitmrjn@users.noreply.github.com>
2025-02-25 04:53:28 +0800

    Add files via upload

---
+++ javascript-course/scripts/10f.js
@@ -0,0 +1,8 @@
+function switchColor (select) {
+  const button = document.querySelector(select);
+    if(!button.classList.contains('is-toggled')) {
+    button.classList.add('is-toggled');
+  } else {
+    button.classList.remove('is-toggled');
+  }
+  }
